d8b2e7a81c2bd9822bd03673684528147a7148aa
Pankajashree <pankajashree@gmail.com>
2017-12-23 08:52:34 +0530

    add title, subtitle, bg, join button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,27 @@
 <template>
-  <div>
+  <div class="content">
     <HeaderBar></HeaderBar>
-    <h1>{{ msg }}</h1>
+    <div class="bg-img">
+      <v-container class="text-xs-center">
+        <h1>{{title}}</h1>
+        <h3>{{subtitle}}</h3>
+        <v-btn color="btn-bg" large class="btn-style" depressed>JOIN US!</v-btn>
+      </v-container>
+    </div>
   </div>
 </template>
 
 <script>
 import HeaderBar from '../components/Header.vue'
 export default {
+  name: 'Home',
   components: {
     HeaderBar
   },
   data () {
     return {
-      msg: 'Code Stories landing page'
+      title: 'Coders of the World',
+      subtitle: 'Story snippets from people learning to code around the world'
     }
   }
 }
@@ -21,18 +29,32 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+.bg-img{
+  background-image: url('~public/images/home_bg.jpg');
+  background-size:cover;
+  background-repeat: no-repeat;
+  height: 576px;
+  display: block;
+}
+h1,h3,.btn-style{
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: #ffffff;
+}
+h1{  
+  font-size: 65px;
+  padding-top: 153px;
+  font-weight: bold;
+}
+h3{
+  font-size: 22.5px;
   font-weight: normal;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.btn-bg{
+  background: #5FD48A;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.btn-style{
+  font-weight: bold;
+  color: #395F5C;
+  margin-top: 50px;
 }
 </style>
