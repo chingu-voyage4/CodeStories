f8b937371ca941d36db50cf03593ac36f2c9e735
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-12-30 23:20:35 +0100

    Fix issue with header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,49 @@
 <template>
-<v-layout row>
+  <v-layout row>
+    <v-navigation-drawer temporary absolute dark class="green darken-4" v-model="drawer" left app>
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>group_add</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>signup</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>group</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>login</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <v-toolbar prominent app color="header-bg" class="header-content" height="75px">
-        <v-flex sm4>
-            <v-toolbar-title>Discover Stories</v-toolbar-title>
-        </v-flex>
-        <v-flex sm4 class="text-xs-center">
-            <v-avatar size="125px">
-                <img class='logo' src="~public/images/CHINGU_LOGO-04.png" alt="CodeStories logo" align="middle" />
-            </v-avatar>
-        </v-flex>
-        <v-flex sm4>
-            <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
-            <v-toolbar-items class="hidden-sm-and-down">
-                <v-btn flat large left>Login</v-btn>
-                <v-btn flat large left>Join</v-btn>
-            </v-toolbar-items>
-        </v-flex>
+      <v-toolbar-side-icon light class="hidden-md-and-up" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-flex sm4>
+        <v-toolbar-title>Discover Stories</v-toolbar-title>
+      </v-flex>
+      <v-flex sm4 class="text-xs-center hidden-sm-and-down">
+        <v-avatar size="125px">
+          <img class='logo' src="~public/images/CHINGU_LOGO-04.png" alt="CodeStories logo" align="middle" />
+        </v-avatar>
+      </v-flex>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn class="buttons" flat large left>Login</v-btn>
+        <v-btn class="buttons" flat large left>Join</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-</v-layout>
+  </v-layout>
 </template>
 
 <script>
 export default {
   data () {
     return {
+      drawer: false
     }
   }
 }
@@ -31,15 +51,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.header-bg{
-    background: #395F5C;
+.header-bg {
+  background: #395f5c;
 }
-.header-content{
-    color: #5FD48A;
-    text-transform: uppercase;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+.header-content {
+  color: #5fd48a;
+  text-transform: uppercase;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
-.logo{
-    margin-top: 75px;    
+.logo {
+  margin-top: 75px;
+}
+.buttons {
+  color: #5fd48a !important;
 }
 </style>
