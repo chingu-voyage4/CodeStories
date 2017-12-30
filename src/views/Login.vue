0583ba72e9142812606f2187315a1f3866449e94
pankaja-shree <pankajashree@gmail.com>
2017-12-30 16:01:31 +0530

    change layout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,56 +1,30 @@
 <template>
-<v-container>
-   <v-flex xs12 sm6 offset-sm3 class="text-xs-center">
-            <v-avatar size="125px">
-                <img class='logo' src="~public/images/CHINGU_LOGO-04.png" alt="CodeStories logo" align="middle" />
-            </v-avatar>
-        </v-flex>
-    <v-container
-      fluid
-      style="min-height: 0;"
-      grid-list-lg
-      class="text-xs-center"
-    >
-      <v-layout row wrap>
-        <v-flex xs12 sm6 offset-sm3>
-          <v-btn block>
-      Login with Gmail
-    </v-btn>
-    <v-btn block>
-      Login with Github
-    </v-btn>
-    <v-btn block>
-      Login with Twitter
-    </v-btn>
-    <v-btn block>
-      Login with Facebook
-    </v-btn>
-    <v-divider inset></v-divider>
-    <v-form>
-    <v-text-field
-      label="E-mail"
-      v-model="user.email"
-      type="email"
-      required
-    ></v-text-field>
-     <v-text-field
-      label="Password"
-      v-model="user.password"
-      type="password"
-      required
-    ></v-text-field>
-    <v-btn block>
-      Login
-    </v-btn>
-  </v-form>
-        </v-flex>
-      </v-layout>
-    </v-container>
-</v-container>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex xs12 sm6>
+            <v-card class="pa-3">
+              <v-flex class="text-xs-center">
+                    <v-avatar size="125px">
+                        <img class='logo' src="~public/images/CHINGU_LOGO-04.png" alt="CodeStories logo" align="middle" />
+                    </v-avatar>
+                </v-flex>
+                <v-btn block outline color="teal">Login with Google</v-btn>
+                <v-btn block outline>Login with Github</v-btn>
+                <v-btn block outline color="indigo">Login with Facebook</v-btn>
+                 <v-btn block outline color="blue">Login with Twitter</v-btn>
+                 <br>
+                <hr>
+                <br>
+                <v-btn block outline color="green">Login</v-btn>
+
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
 </template>
 
 <script>
-import {firebaseConfig} from '../util/firebaseConfig'
+//import {firebaseConfig} from '../util/firebaseConfig'
 
 export default {
   name: 'Login',
