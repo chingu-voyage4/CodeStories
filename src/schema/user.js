6c73b3434dc0712f0f6ff78f6644dc56a1765e57
Ramesh <rameshsyangtan92@gmail.com>
2018-01-12 01:02:59 +0545

    initialize schemas

---
+++ src/schema/user.js
@@ -0,0 +1,17 @@
+/* A schema for user */
+
+export default {
+  uid: "",
+  username: "",
+  firstName: "",
+  lastName: "",
+  name: "",
+  avatar: "",
+  bio: "",
+  socialLinks: [],
+  website: "",
+  stories: [], // Story references
+  draftStories: [], // Draft story references
+  bookmarks: [], // Bookmarked story references
+  hearts: [] // Hearted story references
+};
