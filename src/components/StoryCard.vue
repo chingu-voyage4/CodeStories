a9dfd093461cfb5fc68a69e3a992983e5d94f534
pankaja-shree <pankajashree@gmail.com>
2018-01-16 16:00:59 +0530

    create story card component

---
+++ src/components/StoryCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="content">
+    <HeaderBar></HeaderBar>
+    <div class="bg-img">
+      <v-container class="text-xs-center">
+        <h1>{{title}}</h1>
+        <h3>{{subtitle}}</h3>
+        <router-link to="/register" v-if="!user">
+          <v-btn color="btn-bg" large class="btn-style" depressed> JOIN US! </v-btn>
+        </router-link>
+        <router-link to="/stories" v-else>
+          <v-btn color="btn-bg" large class="btn-style" depressed> DISCOVER STORIES! </v-btn>
+        </router-link>
+      </v-container>
+    </div>
+    <FooterBar></FooterBar>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import HeaderBar from '../components/Header/Header.vue'
+import FooterBar from '../components/Footer.vue'
+export default {
+  name: 'Home',
+  components: {
+    HeaderBar,
+    FooterBar
+  },
+  data () {
+    return {
+      title: 'Coders of the World',
+      subtitle: 'Story snippets from people learning to code around the world'
+    }
+  },
+  computed: mapGetters(["user"]),
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bg-img {
+  background-image: url('~public/images/home_bg.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 576px;
+  display: block;
+}
+h1,
+h3,
+.btn-style {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: #ffffff;
+}
+h1 {
+  font-size: 65px;
+  padding-top: 153px;
+  font-weight: bold;
+}
+h3 {
+  font-size: 22.5px;
+  font-weight: normal;
+}
+.btn-bg {
+  background: #5fd48a !important;
+}
+.btn-style {
+  font-weight: bold;
+  color: #395f5c;
+  margin-top: 50px;
+}
+</style>
