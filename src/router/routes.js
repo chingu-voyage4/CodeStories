e84466c38b34e404b883e77ff69ec777f68220fa
pankaja-shree <pankajashree@gmail.com>
2018-01-16 16:44:26 +0530

    add stories route

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,7 @@ import Test from '../components/Test/Test.vue';
 import Login from '../views/Login/Login.vue';
 import Home from '../views/Home.vue';
 import Signup from '../views/Signup/Signup.vue';
+import Stories from '../views/Stories.vue';
 
 /* Protected routes */
 const protectedRoutes = [
@@ -13,14 +14,14 @@ const publicRoutes = [
   { path: '/', component: Home },
   { path: '/home', component: Home },
   { path: '/register', component: Signup },
+  { path: '/stories', component: Stories },
   {
     path: '/login',
     component: Login,
 
     // Spread route query as props
     props: (route) => ({ ...route.query })
-  },
-  { path: '/stories', component: Stories }
+  }
 ];
 
 export default [
