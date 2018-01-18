b11a7208e7eff0d32ad42b992a97654dd3c9ddb6
pankaja-shree <pankajashree@gmail.com>
2018-01-18 17:31:09 +0530

    create story grid

--- src/components/StoryGrid.vue
+++ src/components/StoryGrid.vue
@@ -1,5 +1,20 @@
 <template>
-<v-container>
-
+<v-container fluid>
+<v-layout>
+  <v-flex xs4 v-for="i in 5" :key="i">
+        <story-card></story-card>
+      </v-flex>
+</v-layout>
 </v-container>
 </template>
+
+<script>
+import StoryCard from '../components/StoryCard.vue'
+
+export default {
+  name: 'StoryGrid',
+  components: {
+    StoryCard
+  }
+}
+</script>
