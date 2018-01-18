793b40a3eb3b4fd1caacaf21d661cbbac8f63891
pankaja-shree <pankajashree@gmail.com>
2018-01-18 15:56:09 +0530

    insert date in card-title

--- src/components/StoryCard.vue
+++ src/components/StoryCard.vue
@@ -6,15 +6,10 @@
         >
         </v-card-media>
         <v-card-title primary-title>
-          <div>
-            <div>
-              <span class="headline">Story title</span>
-              <span class="text-xs-right">Date</span>
-            </div>
-            <span class="grey--text">by Author</span>
-          </div>
+              <span class="headline">Kangaroo Valley Safari</span><v-spacer></v-spacer> <span class="grey--text">Date</span>
         </v-card-title>
           <v-card-text>
+            <p class="grey--text">by Author</p>
             I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
           </v-card-text>
           <v-card-actions>
