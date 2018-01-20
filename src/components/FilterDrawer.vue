c88fb4b0174e33dc7189efe76c2b589f91b8a6ab
pankaja-shree <pankajashree@gmail.com>
2018-01-20 16:41:46 +0530

    insert expansion panels

--- src/components/FilterDrawer.vue
+++ src/components/FilterDrawer.vue
@@ -17,6 +17,18 @@
               bottom chips
             ></v-select>
         </v-list-tile>
+        <v-list-tile>
+          <h5>Top Filters:</h5>
+          <v-expansion-panel expand>
+            <v-expansion-panel-content>
+              <div slot="header">Top Categories</div>
+              <p>Checkboxes</p>
+            </v-expansion-panel-content>
+            <v-expansion-panel-content>
+              <div slot="header">Top Tags</div>
+              <p>Checkboxes</p>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
         </v-list-tile>
       </v-list>
  </v-app>
@@ -25,16 +37,26 @@
 <script>
   export default {
     name: 'FilterDrawer',
+    data () {
+      return {
+        sortby: null,
+        sortitems: [
+          {text: 'Author'},
+          {text: 'Title'},
+          {text: 'Tag'}
+          ]
+      }
+    }
   };
 </script>
 
 <style scoped>
 * {
   font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: #757575;
 }
 h3 {
   font-size: 22.5px;
   font-weight: normal;
-  color: #757575;
 }
 </style>
