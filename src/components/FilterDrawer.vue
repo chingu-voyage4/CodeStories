9e3362701b7fba732cf4950ddcf0c72ddee8dec5
pankaja-shree <pankajashree@gmail.com>
2018-01-20 16:19:38 +0530

    create drawer component

---
+++ src/components/FilterDrawer.vue
@@ -0,0 +1,37 @@
+<template>
+ <v-app>
+      <v-list class="pa-1">
+        <v-list-tile>
+            <h3>Advanced Filter</h3>
+        </v-list-tile>
+      </v-list>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in items" :key="item.title" @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+ </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'FilterDrawer',
+  };
+</script>
+
+<style scoped>
+* {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+h3 {
+  font-size: 22.5px;
+  font-weight: normal;
+  color: #757575;
+}
+</style>
