4f2079121c7bf982b54c23808d69cd5615ef6939
pankaja-shree <pankajashree@gmail.com>
2018-01-20 16:11:44 +0530

    create drawer for advanced filter

--- src/views/Stories.vue
+++ src/views/Stories.vue
@@ -24,6 +24,32 @@
          </v-flex>
          <v-flex text-xs-right>
            <a class="links pa-2" v-for='category in story_categories'>{{category}} </a>
+           <v-btn icon @click.stop="drawer = !drawer">
+                <v-icon color="grey">fa-filter</v-icon>
+            </v-btn>
+            <v-navigation-drawer
+              temporary
+              v-model="drawer"
+              absolute
+              right
+            >
+      <v-list class="pa-1">
+        <v-list-tile>
+            <h3>Advanced Filter</h3>
+        </v-list-tile>
+      </v-list>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-tile v-for="item in items" :key="item.title" @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
          </v-flex>
        </v-layout>
      </v-container>
@@ -50,7 +76,12 @@ export default {
       pageTitle: 'Discover More Stories',
       searchText: '',
       top_tags: ['Origin story', 'Inspiration', 'Life'],
-      story_categories: ['HOT', 'NEWEST', 'MOST', 'VIEWED', 'BOOKMARKED']
+      story_categories: ['HOT', 'NEWEST', 'MOST', 'VIEWED', 'BOOKMARKED'],
+      drawer: null,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ]
     }
   },
   computed: mapGetters(["user"]),
