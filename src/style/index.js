0c70a46cca177c669ce471a97584e93fd20fa902
Ramesh Syangtan <rameshsyangtan92@gmail.com>
2018-01-20 23:12:32 +0545

    add vue medium editor

--- src/style/index.js
+++ src/style/index.js
@@ -1,5 +1,13 @@
 /* External styles */
 
 /* Vuetify */
-import 'vuetify/dist/vuetify.min.css';
-import './styles.css'
+import "vuetify/dist/vuetify.min.css";
+
+/* Medium Editor */
+import "medium-editor/dist/css/medium-editor.css";
+
+// Default theme looks cool :)
+import "medium-editor/dist/css/themes/default.css";
+
+/* Global styles */
+import './styles.css';
