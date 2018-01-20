b414bf96ef2134f15389dc1cd381d05fce792e7e
pankaja-shree <pankajashree@gmail.com>
2018-01-20 16:41:33 +0530

    insert select item

--- src/components/FilterDrawer.vue
+++ src/components/FilterDrawer.vue
@@ -7,13 +7,16 @@
       </v-list>
       <v-list class="pt-1" >
         <v-divider></v-divider>
-        <v-list-tile v-for="item in items" :key="item.title" @click="">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
+        <v-list-tile>
+          <v-subheader v-text="'Sort by'"></v-subheader>
+          <v-select
+              v-bind:items="sortitems"
+              v-model="sortby"
+              label="Select"
+              single-line
+              bottom chips
+            ></v-select>
+        </v-list-tile>
         </v-list-tile>
       </v-list>
  </v-app>
