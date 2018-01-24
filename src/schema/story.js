8aa306db5ec252baed1480d74e9c2f91ce883710
Ramesh Syangtan <rameshsyangtan92@gmail.com>
2018-01-24 22:55:33 +0545

    add story schema

---
+++ src/schema/story.js
@@ -0,0 +1,17 @@
+/* A Schema for a story */
+
+export default {
+  storyUid: "",
+  title: "",
+  coverPhotoPath: "",
+  story: "",
+  createdAt: null,
+  publishAt: null,
+  author: null,
+  isDraft: true,
+  isNew: true,
+  categories: [],
+  tags: [],
+  hearts: [],
+  slug: ""
+};
