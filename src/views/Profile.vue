e0ddd280beea45e5515de33143299b0b4c9b7011
Pankajashree <pankajashree@gmail.com>
2018-01-27 19:30:02 +0530

    create profile component

---
+++ src/views/Profile.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="content">
+    <HeaderBar></HeaderBar>
+    Profile page
+    <FooterBar></FooterBar>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import HeaderBar from '../components/Header/Header.vue'
+import FooterBar from '../components/Footer.vue'
+import StoryGrid from '../components/StoryGrid.vue'
+
+export default {
+  name: 'Profile',
+  components: {
+    HeaderBar,
+    FooterBar
+  },
+  data () {
+    return {
+      title: 'Coders of the World',
+      subtitle: 'Story snippets from people learning to code around the world'
+    }
+  },
+  computed: mapGetters(["user"]),
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.bg-img {
+  background-image: url('~public/images/home_bg.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 576px;
+  display: block;
+}
+h1,
+h3,
+.btn-style {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: #ffffff;
+}
+h1 {
+  font-size: 65px;
+  padding-top: 153px;
+  font-weight: bold;
+}
+h3 {
+  font-size: 22.5px;
+  font-weight: normal;
+}
+.btn-bg {
+  background: #5fd48a !important;
+}
+.btn-style {
+  font-weight: bold;
+  color: #395f5c;
+  margin-top: 50px;
+}
+.neg-margin-top {
+  margin-top: -10%;
+}
+</style>
