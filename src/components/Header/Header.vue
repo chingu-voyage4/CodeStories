2bf4158de4f048a414cf1244f07aada081c7b376
Harshil Agrawal <ghagrawal17@gmail.com>
2018-08-22 04:29:54 +0530

    header buttons enhancement

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-layout row>
-    <v-navigation-drawer temporary absolute dark class="green darken-4" v-model="drawer" left app>
+  <div>
+    <v-navigation-drawer temporary absolute light class="green lighten-4" v-model="drawer" left app>
       <v-list dense v-if="!user">
         <v-list-tile @click="goTo('/login')">
           <v-list-tile-action>
@@ -10,6 +10,7 @@
             <v-list-tile-title>Login</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
+        <v-divider></v-divider>
         <v-list-tile @click="goTo('/register')">
           <v-list-tile-action>
             <v-icon>group_add</v-icon>
@@ -22,47 +23,37 @@
       <UserMenu v-else :user="user" isDrawer="true"></UserMenu>
     </v-navigation-drawer>
     <v-toolbar prominent app color="header-bg" class="header-content" height="75px">
-      <v-toolbar-side-icon light class="hidden-md-and-up" @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-flex sm4>
-        <v-toolbar-title>Discover Stories</v-toolbar-title>
-      </v-flex>
-      <v-flex sm4 class="text-xs-center hidden-sm-and-down">
-        <v-avatar size="125px">
-          <img class='logo' src="~public/images/CHINGU_LOGO-04.png" alt="CodeStories logo" align="middle" />
-        </v-avatar>
-      </v-flex>
+      <!-- The menu button -->
+      <v-toolbar-side-icon light class="hidden-md-and-up buttons" @click.stop="drawer = !drawer"></v-toolbar-side-icon>      
+      <v-toolbar-title>Discover Stories</v-toolbar-title>
+      <v-spacer></v-spacer>      
+      <v-avatar size="125px" class="hidden-sm-and-down">
+        <img class='logo' src="~public/images/CHINGU_LOGO-04.png" alt="CodeStories logo" align="middle" />
+      </v-avatar>
       <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-toolbar-title v-if="user">
-          <v-btn class="buttons" @click="handleEdit" flat small v-if="singleStory.onSinglePage">
-            <v-icon>edit</v-icon> &nbsp;Edit
-          </v-btn>
-          <router-link to="/new">
-            <v-btn class="buttons" flat small>
-              <v-icon>message</v-icon> &nbsp;New story
-            </v-btn>
-          </router-link>
-          <v-menu bottom left offset-y>
-            <v-avatar class="profile" size="45px" slot="activator" v-if="user.photoURL">
-              <img :src="user.photoURL" />
-            </v-avatar>
-            <div v-else slot="activator">
-              <v-icon class="buttons profile" large>person</v-icon>
-            </div>
-            <UserMenu :user="user"></UserMenu>
-          </v-menu>
-        </v-toolbar-title>
-        <div v-else>
-          <router-link to="/login">
-            <v-btn class="buttons" flat small>Login</v-btn>
-          </router-link>
-          <router-link to="/register">
-            <v-btn class="buttons" flat small>Join</v-btn>
-          </router-link>
-        </div>
-        </v-toolbar-items>
+      <v-toolbar-items class="hidden-sm-and-down" v-if="user">
+        <v-btn class="buttons" @click="handleEdit" flat v-if="singleStory.onSinglePage">
+          <v-icon>edit</v-icon> &nbsp;Edit
+        </v-btn>
+        <v-btn class="buttons" flat to="/new">
+          <v-icon>message</v-icon> &nbsp;New story
+        </v-btn>
+        <v-menu bottom left offset-y>
+          <v-avatar class="profile" size="45px" slot="activator" v-if="user.photoURL">
+            <img :src="user.photoURL" />
+          </v-avatar>
+          <div v-else slot="activator">
+            <v-icon class="buttons profile" large>person</v-icon>
+          </div>
+          <UserMenu :user="user"></UserMenu>
+        </v-menu>
+      </v-toolbar-items>
+      <v-toolbar-items class="hidden-sm-and-down" v-else>          
+          <v-btn class="buttons" flat to="/login">Login</v-btn>
+          <v-btn class="buttons" flat to="/register">Join</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-  </v-layout>
+  </div>
 </template>
 
 <script>
